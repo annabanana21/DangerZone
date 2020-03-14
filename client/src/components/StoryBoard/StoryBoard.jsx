2cdfff723f2f6622057df8b84e6c6c85c2824a14
Anna <av.mastoris@gmail.com>
2020-03-14 07:16:04 -0400

    assets edited for clarity and toggle bug fixed

--- client/src/components/StoryBoard/StoryBoard.jsx
+++ client/src/components/StoryBoard/StoryBoard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ToggleButton from '../ToggleButton/ToggleButton';
 import './StoryBoard.scss';
-import clip from '../../assets/Icons/clipboard.png';
+import clip from '../../assets/Icons/clipboard-clip.png';
 
 
 export default class StoryBoard extends React.Component {
@@ -27,7 +27,12 @@ export default class StoryBoard extends React.Component {
                 {
                     this.state.displayBoard &&
                     <div className='story' onClick={() => this.toggle()}>
+                    <div className='story__wrap'>
                         <img src={clip} className='story__board' onClick={this.stopPropagation}/>
+                        <div className='story__write'>
+                            <h3 className='story__title'>CHECKLIST</h3>
+                        </div>
+                    </div>
                     </div>
                 }
                 { !this.state.displayBoard &&
